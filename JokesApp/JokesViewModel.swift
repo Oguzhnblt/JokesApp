b57f3262edba8d7d59a881b71fa76dcc3c4064f8
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-08-14 20:14:30 +0300

    Update JokesViewModel.swift

--- JokesApp/JokesViewModel.swift
+++ JokesApp/JokesViewModel.swift
@@ -12,7 +12,21 @@ class JokesVİewModel : ObservableObject {
     
     @Published var jokes = [Value]()
     
+    init() {
+        getJokes()
+    }
+    
     func getJokes () {
         
+        AF.request("https://raw.githubusercontent.com/atilsamancioglu/JokesAppJsonData/main/chuck.json",method: .get).responseDecodable(of: JokesData.self) { response in
+            
+            switch response.result {
+                case .success(let data):
+                    let value = data.value
+                    self.jokes += value
+                case .failure(let error):
+                    print(error)
+            }
+        }
     }
 }
