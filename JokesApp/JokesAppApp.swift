84153d861d0fd71c06681eb114b6336c6ee027ef
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-08-14 19:39:53 +0300

    Initial Commit

---
+++ JokesApp/JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Oğuz on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
